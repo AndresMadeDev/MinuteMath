c50c866f8bf4663c075e3a719062bec52126ee6d
AndresMadeDev <andres.made.dev@gmail.com>
2023-05-13 21:13:38 -0400

    Initial Commit

---
+++ MinuteMath/MinuteMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinuteMathApp.swift
+//  MinuteMath
+//
+//  Created by Andres Made on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MinuteMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
